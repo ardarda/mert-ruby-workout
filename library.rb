28e90737ec43f729d341890e3c221b70f42debbf
Arda Cicek <ardacck@gmail.com>
2015-06-03 22:28:47 +0300

    yoyo

---
+++ library.rb
@@ -0,0 +1,59 @@
+class Book
+    
+    def initialize(name, author, page_number)
+        @name = name
+        @author = author
+        @page_number = page_number
+    end
+    
+    def to_s
+        "Book: #{@name}--#{@author}--#{@page_number}"
+    end
+    
+end
+
+
+
+def add_book
+	puts "How many books would you like to donate?"
+	n = Integer(gets)
+
+	@books = Array.new n do
+        puts "Name of the book?"
+        name = gets.chomp
+        puts "Author of the book?"
+        author = gets.chomp
+        puts "Page # of the book?"
+        page_number = gets.chomp
+        n = Book.new(name, author, page_number)
+	end
+end
+
+def borrow
+	puts "Enter the name of the book you want to borrow."
+	bor_book = gets.chomp
+	@books.delete(bor_book)
+	
+end
+
+
+def list_book
+puts @books
+end
+
+loop do
+	puts "Welcome to my library!!"
+  	puts %q{Please select an option:
+    1. Donate book
+    2. Borrow book
+    3. List books
+    }
+  	case gets.chomp
+		when '1'
+      	add_book
+    	when '2'
+      	borrow
+    	when '3'
+      	list_book
+	end 
+end
